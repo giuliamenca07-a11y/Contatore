608a02cef8ea744d63ec32e45e72b224b7073f94
giuliamenca07 <giuliamenca07@gmail.com>
2025-11-19 11:28:12 +0100

    modificato

--- src/SynchronizedBlockExample.java
+++ src/SynchronizedBlockExample.java
@@ -12,6 +12,24 @@ public class SynchronizedBlockExample {
         return count;
     }
 
-}
+    public static void main(String[] args) {
+        SynchronizedBlockExample example = new SynchronizedBlockExample();
 
+        // Create multiple threads to increment the counter
+        Thread t1 = new Thread(() -> example.increment());
+        Thread t2 = new Thread(() -> example.increment());
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final count: " + example.getCount());
+    }
+}
 
